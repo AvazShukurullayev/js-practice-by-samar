abd8fa043965285658eef53c522f036d411b0bd8
Avaz <elonarm21@gmail.com>
2024-01-04 01:53:49 +0500

    function js

--- app.js
+++ app.js
@@ -29,3 +29,43 @@
 // }
 //
 // console.log("answers", answers)
+
+
+// if
+// else
+// else if
+// switch case
+
+
+function appFunc(name, age) {
+    console.log(`Hello my name is ${name} and I am full stack developer`)
+
+    function hardSkills() {
+        const skills = ["HTML", "CSS", "JavaScript"]
+        for (let i = 0; i < skills.length; i++) {
+            alert(`I have ${skills[i]} skill.`)
+        }
+    }
+
+    hardSkills()
+
+    function checkAge(age) {
+        if (age > 18) {
+            alert("It is good age to start to learn IT")
+        } else if (age < 18) {
+            alert("You can start to learn IT at you age.")
+        } else {
+            alert("Oops. I am sorry, also you can do it, dude")
+        }
+    }
+
+    checkAge(age)
+
+    function calcPow(number) {
+        return number * number
+    }
+
+    console.log("calcPow => ", calcPow(25))
+}
+
+appFunc("Shaq", 19)
