6c16067f538d21e8ca0005423b6019b0564484e5
Avaz <elonarm21@gmail.com>
2024-01-04 01:28:59 +0500

    js tutorial

---
+++ app.js
@@ -0,0 +1,31 @@
+"use strict"
+
+// let answers = []
+// let questions = ["What is your name?", "What is your phone number?", "What is your address?", "How old are you?"]
+// for (let i = 0; i < questions.length; i++) {
+//     answers[i] = prompt(questions[i])
+// }
+//
+// console.log("answers", answers)
+
+// let answers = []
+// let questions = ["What is your name?", "What is your phone number?", "What is your address?", "How old are you?"]
+//
+// let i = 0
+// do {
+//     answers[i] = prompt(questions[i])
+//     i++
+// } while (i < questions.length)
+//
+// console.log("answers", answers)
+
+// let answers = []
+// let questions = ["What is your name?", "What is your phone number?", "What is your address?", "How old are you?"]
+//
+// let i = 0
+// while (i < questions.length) {
+//     answers[i] = prompt(questions[i])
+//     i++
+// }
+//
+// console.log("answers", answers)
